ada35eafffe8b23fe6ea961b34ac2f0f0556349e
Jose A Dianes <jadianes@gmail.com>
2014-08-30 16:11:39 +0100

    using rCharts

--- server.R
+++ server.R
@@ -1,18 +1,43 @@
 library(shiny)
-library(ggplot2)
+require(rCharts)
 library(devtools)
 install_github(user = "jadianes", repo = "prider")
 
 shinyServer(function(input, output) {
     
-    output$searchResultsPlot <- renderPlot({
+#     output$myChart <- renderChart({
+#         names(iris) = gsub("\\.", "", names(iris))
+#         p1 <- rPlot(input$x, input$y, data = iris, color = "Species", 
+#                     facet = "Species", type = 'point')
+#         p1$addParams(dom = 'myChart')
+#         return(p1)
+#     })
+    
+    output$searchResultsChart <- renderChart({
         projects <- search_projects(input$q, input$numResults)
+        ag.projects <- aggregate(projects[,'numAssays'] ~ projects[,input$x] + projects[,input$group.var], data = projects, sum)
+        names(ag.projects) <- c(input$x, input$group.var, "numAssays")
         if (nrow(projects)>0) {
-            ggplot(projects, aes(x=accession, y=numAssays, fill = submissionType)) +
-                geom_bar(stat="identity") +
-                scale_fill_manual(values = c("yellow", "orange", "red")) +
-                theme_bw() +
-                theme(axis.text.x=element_text(angle=-90, hjust=0))
+            resultsPlot <- nPlot(
+                y = 'numAssays', x = input$x,
+                group = input$group.var,
+                data = ag.projects,
+                type = 'multiBarChart'
+            )
+#             resultsPlot <- rPlot(
+#                 x = 'accession', y = 'numAssays',
+#                 color = 'species', facet = 'submissionType',
+#                 data = projects,
+#                 type = 'point'
+#             )
+            resultsPlot$addParams(dom = 'searchResultsChart')
+            return(resultsPlot)
+            
+#             ggplot(projects, aes(x=accession, y=numAssays, fill = submissionType)) +
+#                 geom_bar(stat="identity") +
+#                 scale_fill_manual(values = c("yellow", "orange", "red")) +
+#                 theme_bw() +
+#                 theme(axis.text.x=element_text(angle=-90, hjust=0))
         }
     })
     
--- ui.R
+++ ui.R
@@ -1,5 +1,6 @@
 # ui.R
 library(shiny)
+require(rCharts)
 
 shinyUI(fluidPage(
     titlePanel("The Proteomics Repository Gateway"),
@@ -8,17 +9,25 @@ shinyUI(fluidPage(
                       fluidRow(
                           textInput("q",label=h4("Search")),
                           
-                          selectInput("sizeVar", 
-                                      label = "Choose count to display",
-                                      choices = c("Assays", "Proteins", "Peptides", "Spectra"),
-                                      selected = "Assays"),
+#                           selectInput("sizeVar", 
+#                                       label = "Choose count to display",
+#                                       choices = c("Assays", "Proteins", "Peptides", "Spectra"),
+#                                       selected = "Assays"),
                           
                           sliderInput("numResults", label = h5("Num results"), min = 10, 
-                                      max = 50, value = 10)
+                                      max = 50, value = 10),
+                          selectInput(inputId = "x",
+                                      label = "Choose X",
+                                      choices = c('species', 'tissues', 'instrumentNames'),
+                                      selected = "species"),
+                          selectInput(inputId = "group.var",
+                                      label = "Choose Grouping",
+                                      choices = c('species', 'tissues', 'instrumentNames'),
+                                      selected = "tissues")
                       )
                   ),
                   mainPanel(
-                      plotOutput("searchResultsPlot")
+                      showOutput("searchResultsChart", "nvd3")
                   )
     )
 ))
