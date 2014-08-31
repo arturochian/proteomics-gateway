5fd821d92b341534788843d6c26b23e89bdea30f
Jose A Dianes <jadianes@gmail.com>
2014-08-31 15:20:53 +0100

    layout refactoring and other major changes

--- server.R
+++ server.R
@@ -5,7 +5,14 @@ source("./R/projects.df.R")
 shinyServer(function(input, output) {
     
     output$searchResultsChart <- renderChart({
-        projects <- search_projects(input$q, input$numResults)
+        projects <- reduceColumnValues(
+            search_projects(input$q, input$numResults),
+            input$x,
+            'numAssays',
+            5,
+            paste('other', input$x)
+        )
+        projects <- reduceColumnValues(projects, input$group.var, 'numAssays', 5, paste('other', input$group.var))
         ag.projects <- aggregate(projects[,'numAssays'] ~ projects[,input$x] + projects[,input$group.var], data = projects, sum)
         names(ag.projects) <- c(input$x, input$group.var, "numAssays")
         if (nrow(projects)>0) {
@@ -15,6 +22,10 @@ shinyServer(function(input, output) {
                 data = ag.projects,
                 type = 'multiBarChart'
             )
+            resultsPlot$xAxis( axisLabel = input$x )
+            resultsPlot$yAxis( axisLabel = "Assay count" )
+            resultsPlot$addParams(width = '1200')
+            resultsPlot$addParams(height = '600')
             resultsPlot$addParams(dom = 'searchResultsChart')
             return(resultsPlot)
         }
@@ -22,3 +33,15 @@ shinyServer(function(input, output) {
     
 })
 
+reduceColumnValues <- function(df, reduce.column.name, count.column.name, max.values, reduce.to) {
+    # get top values
+    agg.df <- aggregate(df[,count.column.name] ~ df[,reduce.column.name],data=df,sum)
+    names(agg.df) <- c(reduce.column.name, count.column.name)
+    agg.df <- agg.df[order(agg.df[,count.column.name], decreasing=TRUE),]
+    top.values <- agg.df[1:min(max.values,nrow(agg.df)),reduce.column.name]
+        
+    # reduce data frame
+    df[,reduce.column.name] <- ifelse((df[,reduce.column.name] %in% top.values), as.character(df[,reduce.column.name]), reduce.to)
+    
+    df
+}
--- ui.R
+++ ui.R
@@ -2,32 +2,38 @@
 require(rCharts)
 
 shinyUI(fluidPage(
-    titlePanel("The Proteomics Repository Gateway"),
-    sidebarLayout(position = "right",
-                  sidebarPanel(
-                      fluidRow(
-                          textInput("q",label=h4("Search")),
+    verticalLayout(    
+        titlePanel("The Proteomics Repository Gateway"),
+        showOutput( "searchResultsChart", "nvd3" ),
+#         wellPanel(
+            fluidRow(
+                column(3,
+                    textInput("q",label=h4("Search"))
+                ),
                           
 #                           selectInput("sizeVar", 
 #                                       label = "Choose count to display",
 #                                       choices = c("Assays", "Proteins", "Peptides", "Spectra"),
 #                                       selected = "Assays"),
                           
-                          sliderInput("numResults", label = h5("Num results"), min = 10, 
-                                      max = 50, value = 10),
-                          selectInput(inputId = "x",
-                                      label = "Choose X",
-                                      choices = c('species', 'tissues', 'instrumentNames'),
-                                      selected = "species"),
-                          selectInput(inputId = "group.var",
-                                      label = "Choose Grouping",
-                                      choices = c('species', 'tissues', 'instrumentNames'),
-                                      selected = "tissues")
-                      )
-                  ),
-                  mainPanel(
-                      showOutput("searchResultsChart", "nvd3")
-                  )
+                column(3,
+                    sliderInput("numResults", label = h5("Num results"), min = 20, 
+                              max = 1000, value = 20)
+                ),
+                column(3,
+                    selectInput(inputId = "x",
+                              label = "Choose X",
+                              choices = c('species', 'tissues', 'instrumentNames'),
+                              selected = "species")
+                ),
+                column(3,
+                    selectInput(inputId = "group.var",
+                              label = "Choose Grouping",
+                              choices = c('species', 'tissues', 'instrumentNames'),
+                              selected = "tissues")
+                )
+            )
+#         )
     )
 ))
 
