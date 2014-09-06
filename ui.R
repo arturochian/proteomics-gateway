d7da44bc6f90b98a7e62dfda87b7541ce53e5664
Jose A Dianes <jadianes@gmail.com>
2014-09-06 17:48:19 +0100

    added about tab

--- ui.R
+++ ui.R
@@ -9,47 +9,48 @@ shinyUI(
             inverse = TRUE,
             collapsable = TRUE,
             tabPanel('Experiments', 
-                     showOutput( "searchResultsChart", "nvd3" )
-             )
-        ),
+                 ## Main view
+                 showOutput( "searchResultsChart", "nvd3" ),
+                 ##  Controls
+                 fluidPage(
+                     fluidRow(
+                         column(3, textInput("q",label=h5("Search")) ),
+                         column(3, uiOutput("num.results.slider") ),
+                         column(2,
+                                selectInput(inputId = "x",
+                                            label = h5("Choose X"),
+                                            choices = c('species', 'tissues', 'instrumentNames'),
+                                            selected = "species"),
+                                checkboxInput(inputId = "hide.other.x", label = "Ignore Others", value = FALSE)
+                         ),
+                         column(2,
+                                selectInput(inputId = "group.var",
+                                            label = h5("Choose Grouping"),
+                                            choices = c('species', 'tissues', 'instrumentNames'),
+                                            selected = "tissues"),
+                                checkboxInput(inputId = "hide.other.group", label = "Ignore Others", value = FALSE)
+                         ),
+                         column(2,
+                                h5("Download results"),
+                                downloadButton('downloadData', 'CSV')
+                         )
+                     ) # fluidRow
+                 ) # fluidPage 
+             ), # Experiments tab
+            tabPanel('About', 
+                 ## Main view
+                 h4("About the Proteomics Gateway"),
+                 p("
+                    Welcome! This is a portal for visual search and statistics of proteomics data. It gathers experimental
+                   metadata from different repositories (right now we just use PRIDE) into meaninful and interactive charts.
+                   "),
+                 p("
+                    Please, feel free to play around with the different controls in each of the views. Remember that you can 
+                   always export your results in CSV format for later use!")
+            ) # About tab
+        )
         
-        ##  Controls
-        fluidPage(
-            fluidRow(
-                column(3,
-                    textInput("q",label=h5("Search"))
-                ),
-                          
-#                           selectInput("sizeVar", 
-#                                       label = "Choose count to display",
-#                                       choices = c("Assays", "Proteins", "Peptides", "Spectra"),
-#                                       selected = "Assays"),
-                          
-                column(3,
-#                     sliderInput("num.results", label = h5("Num results"), min = 20, 
-#                               max = 1000, value = 20),
-                    uiOutput("num.results.slider")
-                ),
-                column(2,
-                    selectInput(inputId = "x",
-                              label = h5("Choose X"),
-                              choices = c('species', 'tissues', 'instrumentNames'),
-                              selected = "species"),
-                    checkboxInput(inputId = "hide.other.x", label = "Ignore Others", value = FALSE)
-                ),
-                column(2,
-                    selectInput(inputId = "group.var",
-                              label = h5("Choose Grouping"),
-                              choices = c('species', 'tissues', 'instrumentNames'),
-                              selected = "tissues"),
-                    checkboxInput(inputId = "hide.other.group", label = "Ignore Others", value = FALSE)
-                ),
-                column(2,
-                       h5("Download results"),
-                       downloadButton('downloadData', 'CSV')
-                )
-            ) # fluidRow
-        ) # fluidPage
+        
     ) # verticalLayout
 ) 
 
